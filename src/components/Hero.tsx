30b4f2bfbf07cb1b5ec7dc9c4150bc5a690a4fb9
Devansh Bansal <devanshbansal500@gmail.com>
2025-06-16 23:27:48 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -62,8 +62,9 @@ const Hero = () => {
               View My Work
             </button>
            <a
-            href="/Resume (3).pdf"
+            href="/Devansh_Bansal_CV.pdf"
             download
+            type="application/pdf"
             className="px-6 py-3 bg-blue-700 text-white rounded-lg hover:bg-blue-800"
             >
             Download CV
