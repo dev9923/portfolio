79837b4000f499b8d13f54026a8aa15ffc2bdf4c
Devansh Bansal <devanshbansal500@gmail.com>
2026-02-18 20:25:25 +0530

    Updated index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Features from './components/Features';
-import LiveMarket from './components/LiveMarket';
-import PredictionDashboard from './components/PredictionDashboard';
-import AIInsights from './components/AIInsights';
-import SentimentAnalysis from './components/SentimentAnalysis';
-import PortfolioOptimizer from './components/PortfolioOptimizer';
-import Pricing from './components/Pricing';
-import TradingPartners from './components/TradingPartners';
-import Footer from './components/Footer';
-import { Toaster } from 'react-hot-toast';
+import About from './components/About';
+import Skills from './components/Skills';
+import Certifications from './components/Certifications';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Toaster position="top-right" />
+    <div className="min-h-screen bg-white">
       <Header />
       <Hero />
-      <Features />
-      <LiveMarket />
-      <PredictionDashboard />
-      <AIInsights />
-      <SentimentAnalysis />
-      <PortfolioOptimizer />
-      <Pricing />
-      <TradingPartners />
-      <Footer />
+      <About />
+      <Skills />
+      <Certifications />
+      <Projects />
+      <Contact />
     </div>
   );
 }
